27f88c77c5978cc06cb8f1307a76ab957e75e9b0
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-03-31 20:52:43 +0530

    Update FlyingBird.jsx

--- src/components/FlyingBird.jsx
+++ src/components/FlyingBird.jsx
@@ -10,7 +10,7 @@ const FlyingBird = () => {
 	});
 
 	// Horizontal (x) movement - right to left
-	const x = useTransform(scrollYProgress, [0, 0.5, 0.9, 1], ["-150%", "70%", "150%", "180%"]);
+	const x = useTransform(scrollYProgress, [0, 0.5, 0.9, 1], ["-150%", "70%", "151%", "180%"]);
 
 	// Vertical (y) movement - controlled descent
 	const y = useTransform(scrollYProgress, [0, 0.5, 0.9, 1], ["0%", "10%", "20%", "15%"]);
